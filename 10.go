13cfacacb03b7ada0b9d0096223cf52e3945a601
Sol <it.is.yurij@gmail.com>
2024-02-22 06:21:24 +1000

    In work

--- 10.go
+++ 10.go
@@ -4,9 +4,24 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	tSlice := []float32{-25.4, -27.4, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	fmt.Println(tSlice)
+	temperatures := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	groups := make(map[int][]float64)
+
+	for _, temp := range temperatures {
+		// Определяем ключ диапазона
+		key := int(math.Floor(temp/10)) * 10
+		// Добавляем температуру в соответствующую группу
+		groups[key] = append(groups[key], temp)
+	}
+
+	// Выводим результат
+	for key, group := range groups {
+		fmt.Printf("Диапазон %d - %d: %v\n", key, key+10, group)
+	}
 }
